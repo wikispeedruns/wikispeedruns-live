e27dc768cc7ec582348eeab0bb3129f1bf687a44
Daniel Qian <daniel.qian@nyu.edu>
2024-02-26 19:51:15 -0500

    finish websocket serve design

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ them.
 Client intially sends a message upon getting ready to play the prompt
 ```
 {
-    "type": "join",
+    "type": "play",
     "user": "dan",
     "lobby_id": 10,
     "prompt_id": 5,
@@ -27,28 +27,19 @@ Client intially sends a message upon getting ready to play the prompt
 ```
 
 If this is the first client, the server creates an entry for this lobby prompt,
-which includes an expiration date and a array indicating which users can force the game to 
-start. It returns a list of players.
+which includes an array indicating which users are ready and which can force 
+the game to start. It returns a list of players. TODO, currently, any user can
+start the game
 
 Clients can send messages of the form
 ```
 {
-    "type": "ready",
     "ready": true, // or false
-    "user": "dan",
-    "lobby_id": 10,
-    "prompt_id": 5,
 }
 ```
 to indicate whether they are ready to start the prompt. If all clients indicate they 
 are ready, then the prompt starts. The server broadcasts this message to other clients
 in the lobby so they can update the list of who isn't ready.
-
-The admin of the lobby and the first user to enter the prompt also have the option
-to 
-
-
-TODO this doesn't handle duplicate user names very well
 
 
 ### 2. Lobby Prompt Leaderboard Finishing
@@ -72,7 +63,7 @@ TODO: show partial prompts too! And refresh on click instead.
 
 ```
 {
-    "type": "finished"
+    "type": "update"
     "lobby_id": 10,
     "prompt_id": 5, 
 }
--- server.py
+++ server.py
@@ -4,36 +4,101 @@ import json
 from websockets.server import serve
 from websockets import broadcast
 
-lobbies = {}
 
-async def sub_to_lobby(websocket):
+# Data structure for lobbys, keyed by (lobby_id, prompt_id)
+prompts = {}
+
+# Data structure for leaderboard subscriptions, keyed by (lobby_id, prompt_id)
+leaderboards = {}
+
+
+async def wait_for_start(ws, lobby_id, prompt_id, user):
+    key = (lobby_id, prompt_id)
+
+    if key not in prompts:
+        prompts[key] = {
+            "usersReady": {},
+            "connections": set()
+        } # TODO don't use a dict here lol
+
+    # Dumb hack to handle multiple users with the same username,
+    # just change their username to "also user" for the user list
+    # Will just keep on appending "also" # TODO maybe something better
+    while user in prompts[key]["usersReady"]:
+        # Rename to 
+        user = "also " + user
+
+    prompts[key]["connections"].add(ws)
+    prompts[key]["usersReady"][user] = False # ready status
+
+    broadcast(prompts[key]["connections"], json.dumps(prompts[key]["usersReady"])) 
+
+    try:
+        while True:
+            msg = ws.recv()
+            msg = json.loads(msg)
+            prompts[key]["usersReady"][user] = msg["ready"]
+
+            if all(prompts[key]["usersReady"].values()):
+                broadcast(prompts[key]["connections"], "start") 
+            else:
+                broadcast(prompts[key]["connections"], json.dumps(prompts[key]["usersReady"])) 
+            
+    finally:
+        prompts[key]["connections"].remove(ws)
+        del prompts[key]["usersReady"][user]
+
+        if len(prompts[key]["connections"]) == 0:
+            del prompts[key]
+    
+    
+
+async def sub_to_leaderboard(ws, lobby_id, prompt_id):
+    key = (lobby_id, prompt_id)
+    if key not in leaderboards:
+        leaderboards[key] = set()
+
+    leaderboards[key].add(ws)
+
+    try:
+        # await websocket.send(f"ack {lobby_id}/{prompt_id}")
+        await ws.wait_closed()
+
+    finally:
+        leaderboards[key].remove(ws)
+        if len(leaderboards[key] == 0):
+            del leaderboards[key]
+
+
+async def update_leaderboard(ws, lobby_id, prompt_id):
+    key = (lobby_id, prompt_id)
+    if len(leaderboards[key]) > 0:
+        await broadcast(leaderboards[key], "refresh")
+    await ws.close()
+
+
+async def connect(websocket):
     message = await websocket.recv()
     message = json.loads(message)
+
+    # TODO exceptions?
+    msgType = message["type"]
     lobby = message["lobby_id"]
     prompt = message["prompt_id"]
 
-    key = (lobby, prompt)
-    if key not in lobbies:
-        lobbies[key] = set()
+    if msgType == "play":
+        user = message["user"]
+        await wait_for_start(websocket, lobby, prompt, user)
+    elif msgType == "leaderboard":
+        await sub_to_leaderboard(websocket, lobby, prompt)
+    elif msgType == "update":
+        await update_leaderboard(websocket, lobby, prompt)
+    else:
+        await websocket.close(reason="invalid message type")
 
-
-    print(key)
-    print(lobbies[key])
-    # Notify other people waiting for lobby leaderboard of new entry
-    if len(lobbies[key]) > 0:
-        broadcast(lobbies[key], "refresh")
-
-    try:
-        lobbies[key].add(websocket)
-
-        await websocket.send(f"ack {lobby}/{prompt}")
-        await websocket.wait_closed()
-
-    finally:
-        lobbies[key].remove(websocket)
 
 async def main():
-    async with serve(sub_to_lobby, "localhost", 9000):
+    async with serve(connect, "localhost", 9000):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
