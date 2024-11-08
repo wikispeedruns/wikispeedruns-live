2499979aeda7c077897b882f397d898e3c07b8be
Daniel Qian <daniel.qian@nyu.edu>
2024-11-08 03:20:15 -0500

    get start working

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from websockets.server import serve
 from websockets import broadcast
 
 
-# Data structure for lobbys, keyed by (lobby_id, prompt_id)
+# Data structure for lobby starts, keyed by (lobby_id, prompt_id)
 prompts = {}
 
 # Data structure for leaderboard subscriptions, keyed by (lobby_id, prompt_id)
@@ -15,42 +15,53 @@ leaderboards = {}
 async def wait_for_start(ws, lobby_id, prompt_id, user):
     key = (lobby_id, prompt_id)
 
+    print(f"{user} waiting to start for Lobby #{lobby_id} Prompt #{prompt_id}")
+
     if key not in prompts:
         prompts[key] = {
-            "usersReady": {},
+            "users": set(),
             "connections": set()
         } # TODO don't use a dict here lol
 
     # Dumb hack to handle multiple users with the same username,
     # just change their username to "also user" for the user list
-    # Will just keep on appending "also" # TODO maybe something better
-    while user in prompts[key]["usersReady"]:
-        # Rename to 
+    # Will just keep on prepending "also" # TODO maybe something better
+    # Note if we don't do this we could accidentally delete users twice
+    while user in prompts[key]["users"]:
         user = "also " + user
 
     prompts[key]["connections"].add(ws)
-    prompts[key]["usersReady"][user] = False # ready status
+    prompts[key]["users"].add(user) # ready status
 
-    broadcast(prompts[key]["connections"], json.dumps(prompts[key]["usersReady"])) 
+    broadcast(prompts[key]["connections"], json.dumps({
+        "type": "update",
+        "users": list(prompts[key]["users"])
+    }))
 
     try:
         while True:
-            msg = ws.recv()
+            msg = await ws.recv()
             msg = json.loads(msg)
-            prompts[key]["usersReady"][user] = msg["ready"]
 
-            if all(prompts[key]["usersReady"].values()):
-                broadcast(prompts[key]["connections"], "start") 
-            else:
-                broadcast(prompts[key]["connections"], json.dumps(prompts[key]["usersReady"])) 
+            # TODO enforce that this only happens for host
+            if (msg["type"] == "start"):
+                broadcast(prompts[key]["connections"], json.dumps({"type": "start"})) 
             
     finally:
         prompts[key]["connections"].remove(ws)
-        del prompts[key]["usersReady"][user]
+        prompts[key]["users"].remove(user)
 
         if len(prompts[key]["connections"]) == 0:
             del prompts[key]
+
+        else: 
+            # If there are still players in the lobby, update list of players
+            broadcast(prompts[key]["connections"], json.dumps({
+                "type": "update",
+                "users": list(prompts[key]["users"])
+            }))
     
+
     
 
 async def sub_to_leaderboard(ws, lobby_id, prompt_id):
@@ -61,10 +72,9 @@ async def sub_to_leaderboard(ws, lobby_id, prompt_id):
 
     leaderboards[key].add(ws)
 
-    # print(f"{len(leaderboards[key])} subbed to Lobby #{lobby_id} Prompt #{prompt_id}")
+    print(f"{len(leaderboards[key])} subbed to Lobby #{lobby_id} Prompt #{prompt_id}")
 
     try:
-        # await websocket.send(f"ack {lobby_id}/{prompt_id}")
         await ws.wait_closed()
 
     finally:
@@ -88,12 +98,13 @@ async def connect(websocket):
     message = await websocket.recv()
     message = json.loads(message)
 
+    print(message)
     # TODO exceptions?
     msgType = message["type"]
     lobby = message["lobby_id"]
     prompt = message["prompt_id"]
 
-    if msgType == "play":
+    if msgType == "wait_start":
         user = message["user"]
         await wait_for_start(websocket, lobby, prompt, user)
     elif msgType == "leaderboard":
@@ -101,7 +112,7 @@ async def connect(websocket):
     elif msgType == "update":
         await update_leaderboard(websocket, lobby, prompt)
     else:
-        print("Invalid Message type: '{msgType}'")
+        print(f"Invalid Message type: '{msgType}'")
         await websocket.close(reason="invalid message type")
 
 
